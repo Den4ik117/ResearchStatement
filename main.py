18292c64c0e9a9e7f72f9ec4c8fdc893d44d0563
Denis <gaz91.91@mail.ru>
2022-11-09 22:54:11 +0500

    Графики количества студентов в определённой группе зелёных баллов

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from research.users import Users
 from jinja2 import Environment, FileSystemLoader
 from research.writer import write
 from datetime import datetime
+from var_dump import var_dump
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 if __name__ == '__main__':
@@ -10,6 +13,46 @@ if __name__ == '__main__':
     template = 'report_template.html'
 
     users = Users.import_from_csv(filename)
+
+    nested_users = users.group('total').sort_keys(reverse=False)
+
+    labels1 = nested_users.keys()
+    values1 = [len(users) for users in nested_users.values()]
+
+    nested_users.sort_values()
+    labels2 = nested_users.keys()
+    values2 = [len(users) for users in nested_users.values()]
+
+    x = np.arange(len(labels1))
+    width = 0.8
+    ylabels = np.arange(0, values2[0] + 20, 20)
+
+    fig, (ax1, ax2) = plt.subplots(2, 1)
+
+    ax1.bar(x, values1, width, label=labels1)
+    ax1.set_title('Диаграмма количества студентов и их баллов')
+    ax1.set_ylabel('Количество студентов')
+    ax1.set_xlabel('Количество зелёных баллов')
+    [ax1.text(labels1[i], values1[i] + 1, values1[i], ha='center', fontsize=6) for i in range(len(labels1))]
+    ax1.set_xticks(x, labels1, fontsize=6)
+    ax1.set_yticks(ylabels, ylabels, fontsize=8)
+    ax1.grid(axis='y')
+
+    ax2.bar(x, values2, width, label=labels2)
+    ax2.set_title('Диаграмма количества студентов и их баллов')
+    ax2.set_ylabel('Количество студентов')
+    ax2.set_xlabel('Количество зелёных баллов')
+    [ax2.text(labels1[i], values2[i] + 1, values2[i], ha='center', fontsize=6) for i in range(len(labels2))]
+    ax2.set_xticks(x, labels2, fontsize=6)
+    ax2.set_yticks(ylabels, ylabels, fontsize=8)
+    ax2.grid(axis='y')
+
+    fig.tight_layout()
+
+    plt.savefig('images/image_1.png')
+
+    # var_dump(nested_users)
+    exit()
 
     env = Environment(loader=FileSystemLoader(template_folder))
     template = env.get_template(template)
--- research/users.py
+++ research/users.py
@@ -34,3 +34,44 @@ class Users:
 
     def get(self, count):
         return Users(self._users[:count])
+
+    def group(self, key):
+        group_users = GroupUsers()
+        for user in self._users:
+            group_users[getattr(user, key)] = user
+        return group_users
+
+
+class GroupUsers:
+    def __init__(self):
+        self._users: dict[str, Users] = {}
+
+    def __setitem__(self, key: str, value: User) -> None:
+        if key in self._users:
+            self._users[key].append(value)
+        else:
+            self._users[key] = Users([value])
+
+    def __getitem__(self, item: str) -> Users:
+        return self._users[item]
+
+    def __len__(self):
+        return len(self._users.keys())
+
+    def keys(self) -> list[str]:
+        return list(self._users.keys())
+
+    def values(self) -> list[Users]:
+        return list(self._users.values())
+
+    def sort_keys(self, reverse=True):
+        tuple_group_users = [(key, values) for key, values in self._users.items()]
+        tuple_group_users.sort(key=lambda users: users[0], reverse=reverse)
+        self._users = dict(tuple_group_users)
+        return self
+
+    def sort_values(self, reverse=True):
+        tuple_group_users = [(key, values) for key, values in self._users.items()]
+        tuple_group_users.sort(key=lambda users: len(users[-1]), reverse=reverse)
+        self._users = dict(tuple_group_users)
+        return self
